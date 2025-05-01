a01345c3b8cced2dd0d866b4bd1936e45bd4a315
dhanushd-27 <dhanushsai2005@gmail.com>
2025-05-01 23:32:00 +0530

    Vars and Consts in Go

---
+++ variables_and_constants/vars_and_const.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Variables
+	var name string = "John Doe"
+	var age int = 30
+	var height float64 = 5.9
+
+	// Constants
+	const pi float64 = 3.14
+	const gravity float64 = 9.81
+
+	// Printing variables and constants
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Pi:", pi)
+	fmt.Println("Gravity:", gravity)
+}
