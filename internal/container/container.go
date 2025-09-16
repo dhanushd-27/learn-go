16076a7f27b0b425278e389b59919d4d57a26b57
dhanushd-27 <dhanushsai2005@gmail.com>
2025-09-16 23:32:24 +0530

    Create DI Container

--- internal/container/container.go
+++ internal/container/container.go
@@ -1,1 +1,17 @@
 package container
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Container struct {
+	db *pgxpool.Pool
+}
+
+// func NewContainer() (*Container, error) {
+// 	// Write code here to connect with db
+// }
+
+func (dc *Container) GetDB() *pgxpool.Pool {
+	return dc.db
+}
